f95bec588384cf68cec18803779bde66b84060bb
westb <westboudreaux@gmail.com>
2026-02-10 18:11:39 -0600

    added more photos for buttons

--- script.js
+++ script.js
@@ -6,6 +6,10 @@
     notWork.css("background", "yellow")
     notWork.css("font-family", "Honk")
     notWork.css("font-size", "20px")
+    var sadFace =$("<img>")
+    sadFace.attr("src", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sad-face_ico.png/640px-Sad-face_ico.png-10-24-12-38-31-576x720.png")
+    sadFace.css("height", "150px")
+    sadFace.css("width", "150px")
 
 
   
@@ -28,22 +32,29 @@
 
     $("#shonen-button").on("click", function() {
         $("#shonen-button").append(notWork)
+        $("#shonen-button").append(sadFace)
     });
        
     $("#shojo-button").on("click", function() {
         $("#shojo-button").append(notWork)
+        $("#shojo-button").append(sadFace)
     });
         
     $("#seinen-button").on("click", function() {
         $("#seinen-button").append(notWork)
+        $("#seinen-button").append(sadFace)
     });
        
     $("#josei-button").on("click", function() {
         $("#josei-button").append(notWork)
+        $("#josei-button").append(sadFace)
+
     });
 
     $("#kodomomuke-button").on("click", function() {
         $("#kodomomuke-button").append(notWork)
+        $("#josei-button").append(sadFace)
+
     });
 
     
